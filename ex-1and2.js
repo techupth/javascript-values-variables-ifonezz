4631a77d084520c30f3a515ccfd2d30b9c043053
Thanawan Wongchanasri <thanawan@orcsoft.co.th>
2024-03-25 23:48:16 +0700

    push the assignment JavaScript Values Variables

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let firstName = "Kerter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const netPrice = 15000;
+const activeRestaurantAmount = 1500;
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+netPrice = 5000;
+activeRestaurantAmount = 2500;
